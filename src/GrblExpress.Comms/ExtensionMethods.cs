aaff7a2300153aa6af3ea7a562ce7fffc588ed30
ByronAP <7310089+ByronAP@users.noreply.github.com>
2025-01-28 22:30:32 -0500

    Fix acknowledgment byte values in GetAckType method
    Updated byte arrays for "Ok" and "Error" statuses in the
    GetAckType method of the GrblExpress.Comms namespace to
    correctly reflect the acknowledgment messages from the
    GRBL system.

--- src/GrblExpress.Comms/ExtensionMethods.cs
+++ src/GrblExpress.Comms/ExtensionMethods.cs
@@ -8,9 +8,11 @@ namespace GrblExpress.Comms
         {
             var span = data.Span;
 
-#pragma warning disable IDE0230 // Use UTF-8 string literal
+#pragma warning disable IDE0230 // Use UTF-8 string literal, GRBL uses ascii
             if (span.IndexOf(new byte[] { 111, 107 }) >= 0) return GrblCommandAckStatus.Ok;
+            if (span.IndexOf(new byte[] { 79, 75 }) >= 0) return GrblCommandAckStatus.Ok;
             if (span.IndexOf(new byte[] { 101, 114, 114, 111, 114 }) >= 0) return GrblCommandAckStatus.Error;
+            if (span.IndexOf(new byte[] { 69, 82, 82, 79, 82 }) >= 0) return GrblCommandAckStatus.Error;
 #pragma warning restore IDE0230 // Use UTF-8 string literal
 
             return GrblCommandAckStatus.Unknown;
